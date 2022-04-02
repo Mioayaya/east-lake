0a5ffdd485f35529640635c1a46eaf2e822a39ac
raften <1530020617@qq.com>
2022-04-03 00:46:36 +0800

    user模型

--- node-app/modles/Users.js
+++ node-app/modles/Users.js
@@ -2,25 +2,36 @@ const Sequelize = require("sequelize");
 const db = require("../database/db");
 
 module.exports = db.sequelize.define('users',{
+        id:{
+            type: Sequelize.INTEGER, // int类型
+            primaryKey:true,    // 主键
+            autoIncrement:true,  // 自增
+        },
         name:{
-            type: String,
-            required: true
+            type: Sequelize.STRING,  // 字符串类型
+            // required: true
         },
         email:{
-            type: String,
-            required: true
+            type: Sequelize.STRING,
+            // required: true
         },
         password:{
-            type: String,
-            required: true
+            type: Sequelize.STRING,
+            // required: true
         },
         avatar:{
-            type: String
+            type: Sequelize.STRING,
         },
         date:{
-            type: Date,
-            default: Date.now
-        },
-})
+            type: Sequelize.STRING,
+            // default: Sequelize.NOW  // 当前时间
+            // 采用js里获取cn time
+        }
+    },{
+        timestamps:false, // 不自动加上createdAt和updateAT
+    },{
+        define: { freezeTableName: true } 
+    }
+)
 
 // module.exports = User = mysql.model("users",UserSchema);
