1af47bc96cb6aad1d3d45dda2adab0954d924f1e
raften <1530020617@qq.com>
2022-04-05 23:53:03 +0800

    注册

---
+++ createapp/src/views/RegisterView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="register">
+        <top-nav></top-nav>
+        <register-body></register-body>
+    </div>
+  
+</template>
+
+<script>
+import TopNav from '@/components/homeview/topNav.vue'
+import RegisterBody from '@/components/registerview/registerBody.vue'
+
+export default {
+    name:'RegisterView',
+    components:{
+        TopNav,RegisterBody
+
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
