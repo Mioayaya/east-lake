a56dd4f040752f633c64e6ea6224a6355e005518
raften <1530020617@qq.com>
2022-04-05 23:53:10 +0800

    登录

---
+++ createapp/src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="login">
+        <top-nav></top-nav>
+        <login-body></login-body>
+    </div>
+</template>
+
+<script>
+import TopNav from '@/components/homeview/topNav.vue'
+import LoginBody from '@/components/loginview/loginBody.vue'
+
+
+export default {
+    name:'LoginView',
+    components:{
+        TopNav,LoginBody,
+    }
+
+}
+</script>
+
+<style lang="less" scoped>
+
+
+</style>
