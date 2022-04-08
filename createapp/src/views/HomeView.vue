beccd731f2d49f36d6d1b88a266f2a8161c78ae3
raften <1530020617@qq.com>
2022-04-08 21:45:19 +0800

    个人页面

--- createapp/src/views/HomeView.vue
+++ createapp/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="home">
-			<top-nav></top-nav>
+			<top-nav :userid="userdata.id"></top-nav>
 			<swiper-nav></swiper-nav>
 			<course-view></course-view>
 	</div>
@@ -10,12 +10,28 @@
 import TopNav from '@/components/homeview/topNav.vue'
 import SwiperNav from '@/components/homeview/swiperNav.vue'
 import CourseView from '@/components/homeview/courseView.vue'
+import { useRouter, useRoute } from 'vue-router'
+import axios from 'axios'
+import { reactive } from '@vue/reactivity'
 
 export default {
 	name:'HomeView',
 	components: {
 		TopNav,SwiperNav,CourseView,
 	},
+	setup() {
+		const userdata = reactive({
+			id: 0,
+		})
+		const route = useRoute();
+		userdata.id = route.query.id;
+		console.log(`homeview-userdata.id: ${userdata.id}`);
+		// 如果是登录后 即userid是存在的 传递给topnav，使得点击头像跳转到个人页面
+		if(userdata.id) {
+			
+		}
+		return {userdata,}
+	}
 }
 </script>
 
