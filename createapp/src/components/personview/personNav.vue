8319e544c8e121c347227eae88aeb0af828a7bc3
raften <1530020617@qq.com>
2022-04-09 20:11:20 +0800

    tab分页

---
+++ createapp/src/components/personview/personNav.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="personnav">
+        <el-tabs v-model="activeName.value" @tab-click="handleClick" class="items">
+            <el-tab-pane label="主页" name="first">
+                <home-list></home-list>
+            </el-tab-pane>
+            <el-tab-pane label="我的" name="second">我的</el-tab-pane>
+            <el-tab-pane label="个人档案" name="third">个人档案</el-tab-pane>
+            <el-tab-pane label="个人设置" name="fourth">个人设置</el-tab-pane>
+        </el-tabs>
+
+    </div>
+</template>
+
+<script>
+import HomeList from '@/components/personview/itemlist/homeList.vue'
+import { reactive, ref } from '@vue/reactivity'
+export default {
+    name:'PersonNav',
+    components:{
+        HomeList,
+    },
+    setup() {
+        const activeName = reactive({
+            value: 'first',
+        })
+
+        let handleClick = ( tab )=> {
+            console.log(tab);
+        }
+        return { activeName,handleClick}
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.personnav {
+    width: 1130px;
+    height: 65px;
+    background-color: white;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    margin-left: 13%;
+    display: flex;
+    position: relative;
+    .items {
+        position: absolute;
+        margin-top: 14px;
+        :deep(.el-tabs__nav) {
+            padding-bottom: 13px;
+            margin-left: 30px;
+        }
+        :deep(.el-tabs__item) {
+            font-size: 20px;
+        }
+    }
+}
+</style>
