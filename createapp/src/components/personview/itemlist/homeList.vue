e1a2d08eee30f259346c1e17a3b36f0e710c90ae
raften <1530020617@qq.com>
2022-04-10 23:10:17 +0800

    个人主页

--- createapp/src/components/personview/itemlist/homeList.vue
+++ createapp/src/components/personview/itemlist/homeList.vue
@@ -1,13 +1,24 @@
 <template>
     <!-- 主页list -->
     <div class="homelist">
-        <div class="left">
+        <!-- 初始页面 -->
+        <div class="left" v-if="userdata.state">
             <div class="desc">
                 <span class="top">个人介绍</span>
                 <span class="sign">{{userdata.sign}}</span>
             </div>
             <div class="edit">
-                <button type="button" class="btn btn-outline-primary">编辑</button>
+                <button type="button" class="btn btn-outline-primary" @click="editSign">编辑</button>
+            </div>
+        </div>
+        <!-- 编辑页面 -->
+        <div class="left" v-if="!userdata.state">
+            <div class="desc">
+                <span class="top">编辑个人介绍</span>
+                <input type="text" class="form-control" id="exampleFormControlInput1" v-model="userdata.sign">
+            </div>
+            <div class="save">
+                <button type="button" class="btn btn-outline-primary" @click="saveSign">保存</button>
             </div>
         </div>
         <div class="right">
@@ -37,6 +48,7 @@ export default {
             id: 0,
             date: '',
             sign: '',
+            state: true,
         })
         userdata.id = store.state.userid;
         if(userdata.id) {
@@ -49,7 +61,16 @@ export default {
             })
         }
         console.log(userdata);
-        return {userdata}
+        let editSign = () => {
+            userdata.state = !userdata.state ;
+        }
+        let saveSign = () => {
+            axios.post('http://localhost:5000/api/v1/editsign',userdata).then ( res => {
+                console.log(res);
+            })
+            userdata.state = !userdata.state ;
+        }
+        return {userdata,editSign,saveSign}
     }
 }
 </script>
@@ -67,6 +88,10 @@ export default {
         .desc {
             display: flex;
             flex-direction: column;
+            .form-control {
+                margin-top: 15px;
+                margin-left: 20px;
+            }
             .top {
                 font-size: 20px;
                 font-weight: 600;
@@ -77,6 +102,10 @@ export default {
             }
         }
         .edit {
+            margin-top: 15px;
+            margin-right: 20px;
+        }
+        .save {
             margin-top: 15px;
             margin-right: 20px;
         }
