626863de5ca97a106bf0000b1228d1740c4fefda
raften <1530020617@qq.com>
2022-04-11 23:08:08 +0800

    论坛

---
+++ createapp/src/views/ForumView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="forrum">
+        <top-nav></top-nav>
+        <forum-top></forum-top>
+        <forum-com></forum-com>
+    </div>
+</template>
+
+<script>
+import TopNav from '@/components/homeview/topNav.vue'
+import ForumTop from '@/components/forumview/forumTop.vue'
+import ForumCom from '@/components/forumview/forumCom.vue'
+export default {
+    name:'ForumView',
+    components:{
+        TopNav,ForumTop,ForumCom
+    },
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
