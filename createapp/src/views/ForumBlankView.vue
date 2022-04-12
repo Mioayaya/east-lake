86182e35364c8957c2d52eac2cd4197ac2adb706
raften <1530020617@qq.com>
2022-04-12 22:08:57 +0800

    空白页面

---
+++ createapp/src/views/ForumBlankView.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- 空白页面 -->
+</template>
+
+<script>
+import { useRouter } from 'vue-router'
+export default {
+    name:'ForumBlankView',
+    setup() {
+        const router = useRouter();
+        router.push({path:'/forumview'});
+        return{router}
+    }
+}
+</script>
+
+<style>
+
+</style>
