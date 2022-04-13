759ce8ac19f0e21d44999e582bf52833c182182e
raften <1530020617@qq.com>
2022-04-13 22:01:49 +0800

    名世资讯

---
+++ createapp/src/views/TeacherView.vue
@@ -0,0 +1,18 @@
+<template>
+    <top-nav></top-nav>
+    专家讲座
+</template>
+
+<script>
+import TopNav from '@/components/homeview/topNav.vue'
+export default {
+    name:'TeacherView',
+    components:{
+        TopNav,
+    }
+}
+</script>
+
+<style>
+
+</style>
