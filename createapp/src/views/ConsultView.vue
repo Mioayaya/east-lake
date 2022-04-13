6c2b3ce96949a2cd4fc5ebf59462b6343cbb3259
raften <1530020617@qq.com>
2022-04-13 22:02:18 +0800

    一对一咨询

---
+++ createapp/src/views/ConsultView.vue
@@ -0,0 +1,19 @@
+<template>
+    <top-nav></top-nav>
+    <consult-top></consult-top>
+</template>
+
+<script>
+import TopNav from '@/components/homeview/topNav.vue'
+import ConsultTop from '@/components/consultview/consultTop.vue'
+export default {
+    name:'ConsultView',
+    components:{
+        TopNav,ConsultTop
+    }
+}
+</script>
+
+<style>
+
+</style>
