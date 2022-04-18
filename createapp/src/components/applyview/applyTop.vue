e39ebc2598d2356f473892580ed821555eaca794
raften <1530020617@qq.com>
2022-04-18 23:31:57 +0800

    支付top

---
+++ createapp/src/components/applyview/applyTop.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="applytop">
+        <router-link :to="{path:'/personview'}">
+            <div class="left">{{'< 返回'}}</div>
+        </router-link>
+        
+        <div class="right">个性化选课</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ApplyTop'
+}
+</script>
+
+<style lang="less" scoped>
+.applytop {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    position: relative;
+    .left {
+        background-color: #ddd;
+        border-radius: 3px;
+        padding: 0 5px 0 5px;
+    }
+    .right {
+        display: flex;
+        margin-left: 200px;
+        font-size: 20px;
+        font-weight: 600;
+    }
+}
+</style>
